5427f0de686edfe7929f0b27618b932da5c7aedb
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-11-03 11:41:12 +0100

    Use jsbin instead of codepen

--- examples.js
+++ examples.js
@@ -2,6 +2,7 @@ module.exports = {
   table: {
     penId: "sIztL",
     name: "Table",
-    title: "How to create a table feature to hide and show columns"
+    title: "How to create a table feature to hide and show columns",
+    url: "http://jsbin.com/ABAPaxI/2/embed?html,js,output"
   }
 };
