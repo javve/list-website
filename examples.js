57e51900bec585cb26ede8c68d0bef33fecb6c5f
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-11-13 21:12:11 +0100

    One more example

--- examples.js
+++ examples.js
@@ -1,8 +1,14 @@
 module.exports = {
+  'existing-list': {
+    url: "http://jsbin.com/IMizewev/1/embed?html,js,output",
+    title: "Basic example: Existing list",
+    name: "Existing list"
+  },
   table: {
     penId: "sIztL",
     name: "Table",
     title: "How to create a table feature to hide and show columns",
     url: "http://jsbin.com/ABAPaxI/2/embed?html,js,output"
   }
+
 };
