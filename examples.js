82fbf6db3bcace72b70161ce6c4bdad7333faade
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-11-25 21:11:07 +0100

    Add more examples

--- examples.js
+++ examples.js
@@ -4,11 +4,30 @@ module.exports = {
     title: "Basic example: Existing list",
     name: "Existing list"
   },
+  'existing-list-add': {
+    url: "http://jsbin.com/ULEruBE/4/embed?html,js,output",
+    title: "Basic example: Existing list + add items",
+    name: "Existing list + add"
+  },
+  'new-list': {
+    url: "http://jsbin.com/IzehIxot/3/embed?html,js,output",
+    title: "Basic example: New list",
+    name: "New list"
+  },
+  'add-edit-remove': {
+    name: "Add, edit, remove",
+    title: "Example to showcase the add, edit and remove functionality.",
+    url: "http://jsbin.com/uLeFazU/3/embed?html,js,output"
+  },
   table: {
-    penId: "sIztL",
     name: "Table",
     title: "How to create a table feature to hide and show columns",
     url: "http://jsbin.com/ABAPaxI/2/embed?html,js,output"
+  },
+  'fuzzy-search': {
+    name: "Fuzzy search",
+    title: "Fuzzy search example",
+    url: "http://jsbin.com/IHeCuHe/3/embed?html,output"
   }
 
 };
